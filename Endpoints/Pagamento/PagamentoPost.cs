d2a83da6e6c519aba73c1fec46adb5f7d3cb5a56
Athos Schrapett <athosschrapett@outlook.com>
2022-02-27 22:54:41 -0300

    Metodo de quantidade de notas e moedas

--- Endpoints/Pagamento/PagamentoPost.cs
+++ Endpoints/Pagamento/PagamentoPost.cs
@@ -13,7 +13,7 @@ public class PagamentoPost
     {
         var pagamento = new Pagamento(pagamentoRequest.ValorTotal, pagamentoRequest.ValorPago);
 
-        if (pagamento.ValorTotal > 0 && pagamento.ValorPago > 0)
+        if (pagamento.ValorTotal > 0 && pagamento.ValorPago > pagamento.ValorTotal)
         {
             dbContext.Pagamento.Add(pagamento);
 
@@ -29,9 +29,72 @@ public class PagamentoPost
             return Results.NotFound();
         }
     }
+
     public static double DefineTroco(double valorTotal, double valorPago)
     {
-        double valorTroco = valorPago - valorTotal;
+        List<double> notas = new List<double> { 10.00, 20.00, 50.00, 100.00 };
+        List<double> moedas = new List<double> { 0.01, 0.05, 0.10, 0.50 };
+
+        List<double> notasTroco = new List<double>();
+        List<double> moedasTroco = new List<double>();
+
+        double valorTroco = Math.Round(valorPago - valorTotal, 2);
+
+        double trocoRetirado = valorTroco;
+        double trocoParaSepararNotas = trocoRetirado;
+
+        var index = 0;
+        do
+        {
+            var notasMaiores = notas.Where(x => x <= trocoRetirado);
+
+            foreach (var nota in notasMaiores.Reverse())
+            {
+                if (!trocoParaSepararNotas.ToString().Split(",")[0].EndsWith("0"))
+                {
+                    string trocoConvertido = trocoParaSepararNotas.ToString().Split(",")[0];
+                    trocoParaSepararNotas = Convert.ToDouble(trocoConvertido.Remove(trocoConvertido.Length - 1) + "0");
+                }
+
+                if (nota == (trocoParaSepararNotas - nota) || nota == notas.Where(x => x <= trocoParaSepararNotas).Reverse().First())
+                {
+                    trocoParaSepararNotas -= nota;
+                    trocoRetirado = Math.Round(trocoRetirado - nota, 2);
+                    notasTroco.Add(Math.Round(nota, 2));
+
+                    break;
+                }
+            }
+
+            index++;
+
+        } while (notasTroco.Count() >= index);
+
+        do
+        {
+            var moedasMaiores = moedas.Where(x => x <= trocoRetirado);
+
+            foreach (var moeda in moedasMaiores.Reverse())
+            {
+                if(moeda == (trocoRetirado - moeda) || moeda == moedas.Where(x => x <= trocoRetirado).Reverse().First())
+                {
+                    trocoRetirado -= Math.Round(moeda, 2);
+                    moedasTroco.Add(Math.Round(moeda, 2));
+                    break;
+                }
+            }
+
+            index++;
+
+        } while (moedas.Count() >= index);
+
+        List<string> notasInfo = new List<string>();
+        List<string> moedasInfo = new List<string>();
+
+        foreach (var nota in notasTroco.Distinct())
+            notasInfo.Add($"Troco em Notas de {nota} R$: " + notasTroco.Where(x => x == nota).Count().ToString()) ;
+        foreach (var moeda in moedasTroco.Distinct())
+            moedasInfo.Add($"Troco em Moedas de {moeda} R$: " + moedasTroco.Where(x => x == moeda).Count().ToString());
 
         return valorTroco;
     }
